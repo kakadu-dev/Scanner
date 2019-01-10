27a2f0326ac2d6ba1aa933bf1f27d7a73462fc96
Roman <r.tsarev@kakadu.bz>
2019-01-10 12:50:55 +0300

    version 3

--- app/src/main/java/bz/kakadu/demoapp/MainActivity.kt
+++ app/src/main/java/bz/kakadu/demoapp/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity(), OnBarcodeDetectorListener {
             .setTitle("Barcode")
             .setMessage(
                 "displayValue: ${barcode.displayValue}\n" +
-                        "format: ${barcode.format}"
+                        "format: ${barcode.formatName}"
             )
             .setPositiveButton(android.R.string.ok, null)
             .setOnDismissListener { scanner?.continueScan() }
@@ -39,11 +39,37 @@ class MainActivity : AppCompatActivity(), OnBarcodeDetectorListener {
             .replace(
                 android.R.id.content,
                 ScannerFragment.instance(
-                    overlayColor = 0xAAFF0000.toInt(),
-                    formats = Barcode.EAN_13 or Barcode.QR_CODE
+                    overlayColor = 0xAAFF0000.toInt()//,   formats = Barcode.EAN_13 or Barcode.QR_CODE
                 )
             )
             .addToBackStack(null)
             .commit()
     }
 }
+
+private val Barcode.formatName
+    get() = when (format) {
+        Barcode.ALL_FORMATS -> "ALL_FORMATS"
+        Barcode.CODE_128 -> "CODE_128"
+        Barcode.CODE_39 -> "CODE_39"
+        Barcode.CODE_93 -> "CODE_93"
+        Barcode.CODABAR -> "CODABAR"
+        Barcode.DATA_MATRIX -> "DATA_MATRIX"
+        Barcode.EAN_13 -> "EAN_13"
+        Barcode.EAN_8 -> "EAN_8"
+        Barcode.ITF -> "ITF"
+        Barcode.QR_CODE -> "QR_CODE"
+        Barcode.UPC_A -> "UPC_A"
+        Barcode.UPC_E -> "UPC_E"
+        Barcode.PDF417 -> "PDF417"
+        Barcode.AZTEC -> "AZTEC"
+        Barcode.ISBN -> "ISBN"
+        Barcode.PRODUCT -> "PRODUCT"
+        Barcode.SMS -> "SMS"
+        Barcode.TEXT -> "TEXT"
+        Barcode.WIFI -> "WIFI"
+        Barcode.GEO -> "GEO"
+        Barcode.CALENDAR_EVENT -> "CALENDAR_EVENT"
+        Barcode.DRIVER_LICENSE -> "DRIVER_LICENSE"
+        else -> "Unknown"
+    }
